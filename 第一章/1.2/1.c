5ab3c906bbae9c4bac6f14ac989e9bd4d317e7fa
sivanWu0222 <sivan0222@vip.qq.com>
2017-10-06 13:21:56 +0800

    add files

---
+++ 第一章/1.2/1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main() {
+	
+	int fahr, celsius;				//华摄氏度，摄氏度 
+	int lower, upper, step;
+	lower = 0;						//温度表的下限 
+	upper = 300;					//温度表的上限 
+	step = 20;						//步长 
+	
+	fahr = lower;
+	while ( fahr <= upper ){
+		celsius = 5 * ( fahr - 32 ) / 9;
+		printf("%d\t%d\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+} 
