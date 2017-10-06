c314cf4468730e48019188bca81a41c1566f75bd
sivanWu0222 <sivan0222@vip.qq.com>
2017-10-06 15:01:35 +0800

    add

--- 第一章/1.2/1-3.c
+++ 第一章/1.2/1-3.c
@@ -1,21 +1,15 @@
 #include<stdio.h>
 main () {
-	
+	printf("\t华摄氏度\t摄氏度准换表\n");
 	float fahr, celsius;
 	float lower, upper, step;
-	
 	lower = 0;
 	upper = 300;
 	step = 20;
 	
-	while ( fahr <= upper ) {
+	while(fahr <= upper) {
 		celsius = (5.0/9.0) * (fahr - 32.0);
-		//%3.0f表示至少占3个字符宽度，并且不包括小数点和小数部分 
-		printf("%3.0f %6.1f\n", fahr, celsius);
+		printf("\t%6.0f\t%15.1f\n", fahr, celsius);
 		fahr = fahr + step;
-		
 	}
-	
-	 
-	
 } 
