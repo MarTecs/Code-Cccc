c67109d5b462aa168cbb71a3a46927d62208d8e3
sivanWu0222 <sivan0222@vip.qq.com>
2017-10-07 17:03:59 +0800

    add some files

---
+++ the-programing-of-c/1/1.5/1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main() {
+	
+	
+	//每次读一个字符 getchar(): 从文本流中读取下一个输入字符，并将其作为结果进行返回 
+	//每次写一个字符 putchar()： 
+	
+	
+	char c = getchar();
+	printf("%c\n",c);
+	putchar(getchar());
+	
+	
+	
+	
+} 
