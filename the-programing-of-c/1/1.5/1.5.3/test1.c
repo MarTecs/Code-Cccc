c1f471cea4f0993c8573cd6e52325588e4ff9169
sivanWu0222 <sivan0222@vip.qq.com>
2017-10-08 10:08:42 +0800

    modify some files

---
+++ the-programing-of-c/1/1.5/1.5.3/test1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+//将一个输入复制到输出，并将其中连续的多个空格用一个空格代替 
+main() {
+	
+	int ch;
+	while( (ch = getchar()) != EOF ) {
+		if (ch = ' ') 
+			
+	}
+	
+}
---
+++ the-programing-of-c/1/1.5/1.5.3/test2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+//将一个输入复制到输出，并将其中的制表符替换为\t,把回退符号替换为\b，将反斜杠替换为\\ 
+main() {
+	int ch = 0;
+	/*
+	while( (ch = getchar()) != EOF ) {
+		if(ch = '\t')
+			ch = '\\t';
+		if(ch = '\b')
+			ch = '\\b';
+		if(ch = '\\')
+			ch = '\\\';
+		printf('%c',ch);
+	}
+	*/
+	printf("%d\t", ch);
+	
+} 
