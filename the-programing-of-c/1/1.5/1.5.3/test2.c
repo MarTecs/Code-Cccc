a35ce0f92e6d1616c20015d423e56daf73886c71
sivanWu0222 <sivan0222@vip.qq.com>
2017-10-08 10:31:13 +0800

    modify files

--- the-programing-of-c/1/1.5/1.5.3/test2.c
+++ the-programing-of-c/1/1.5/1.5.3/test2.c
@@ -1,18 +1,23 @@
 #include<stdio.h>
-//将一个输入复制到输出，并将其中的制表符替换为\t,把回退符号替换为\b，将反斜杠替换为\\ 
+/*
+将一个输入复制到输出，并将其中的制表符替换为\t,把回退符号替换为\b，将反斜杠替换为\\ 
+*/
 main() {
-	int ch = 0;
-	/*
+	char ch = 0;
+	
 	while( (ch = getchar()) != EOF ) {
 		if(ch = '\t')
-			ch = '\\t';
+			printf("%c",ch);
+//			ch = '\\t';
 		if(ch = '\b')
-			ch = '\\b';
+			printf("%c",ch);	
+//			ch = '\\b';
 		if(ch = '\\')
-			ch = '\\\';
+			printf("%c",ch);
+//			ch = '\\\\';
 		printf('%c',ch);
 	}
-	*/
-	printf("%d\t", ch);
+	
+	
 	
 } 
