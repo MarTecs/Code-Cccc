8dd2f715abda2c8df2c9fb73a9f107df7185e785
sivanWu0222 <sivan0222@vip.qq.com>
2017-10-08 10:44:18 +0800

    modify

--- the-programing-of-c/1/1.5/1.5.3/test1.c
+++ the-programing-of-c/1/1.5/1.5.3/test1.c
@@ -2,20 +2,22 @@
 /*将一个输入复制到输出，并将其中连续的多个空格用一个空格代替 */
 main() {
 	
-	//使用temp做一个标记，如果第一个为空格，之后没有空格为0。反之加1 
+	/*使用temp做一个标记，如果第一个为空格，之后没有空格为0。反之加1 */
 	int temp = 0;
-	
 	int ch;
+	
 	while( (ch = getchar()) != EOF ) {
-		if(ch == ' ')
+		if(ch == ' '){
 			temp ++;
+			if(temp != 1) {
+				printf("");
+			}
+		}
 		if(ch != ' ' ) {
 			temp = 0;
-			print("%c", ch);
+			printf("%c", ch);
 		}
-			
 	} 
-	
 	printf("\n");
 	
 }
